95155fa64594dea99541630e5d39b9df8697625c
codystringham <cs.negativ@gmail.com>
2015-12-30 12:52:55 -0700

    colors and variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-   root 'welcome#index'
-   post '/text-to-email', to: "welcome#text_to_email", as: :text_to_email
+  root 'welcome#index'
+  post '/text-to-email', to: "welcome#text_to_email", as: :text_to_email
+
+  get "/admin" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign-out" => "clearance/sessions#destroy", as: "sign_out"
+  resource :session, controller: "clearance/sessions", only: [:create]
 
 end
